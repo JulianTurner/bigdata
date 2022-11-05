31a2fb6d0bf2e711145b9224243637bad9a43739
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2022-11-05 14:49:18 +0000

    schrift erkennung

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt install python3-pip -y
-/usr/bin/python3 -m pip install ipykernel
-pip3 install -r requirements.txt
+/usr/bin/python3 -m pip install --upgrade pip
+/usr/bin/python3 -m pip install ipykernel --upgrade
+pip3 install -r requirements.txt --upgrade
